567982d0191d2ce8fe854cef4fdb33b9da991014
jdisitu <jeanclaudedisch@live.dk>
2023-11-01 15:13:40 +0100

    added navigation sidebar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import './App.css';
+import UserSettingsSidebar from './UserSettingsSidebar';
 
 function App() {
   return (
     <div className="App">
+      <UserSettingsSidebar />
+      {/* Other components go here when finished. */}
     </div>
   );
 }
---
+++ src/UserSettingsSidebar.jsx
@@ -0,0 +1,37 @@
+// UserSettingsSidebar.jsx
+
+import React from 'react';
+import './UserSettingsSidebar.css';
+
+const UserSettingsSidebar = () => {
+  return (
+    <nav className="sidebar-nav">
+        <ul>
+            <li>
+                <div id="userSetting">
+                    <h3>USER SETTINGS</h3>
+                    <ul>
+                        <li><a href="#">My Account</a></li>
+                        <li><a href="#">Privacy & Safety</a></li>
+                        <li><a href="#">My Aliases</a></li>
+                        <li><a href="#">My Posts</a></li>
+                        <li><a href="#">Change Password</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <div id="appSetting">
+                    <h3>APP SETTINGS</h3>
+                    <ul>
+                        <li><a href="#">Appearance</a></li>
+                        <li><a href="#">Country and Language</a></li>
+                        <li><a href="#">Notifications</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </nav>
+  );
+}
+
+export default UserSettingsSidebar;
