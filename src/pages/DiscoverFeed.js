e8f79f1061ada322e1a50e3d8e484b0352367e1c
Sofie Buur <82093368+BuurPrime@users.noreply.github.com>
2023-11-02 17:21:10 +0100

    Merge branch 'topBarComponent' into discoverFeed

--- src/pages/DiscoverFeed.js
+++ src/pages/DiscoverFeed.js
@@ -4,10 +4,12 @@ import "./DiscoverFeed.css"; // The corresponding CSS file
 //Components
 import FeedNavigation from '../components/FeedNavigation';
 import Feed from '../components/Feed';
+import TopBar from '../components/topBarComponent';
 
 function DiscoverFeed() {
   return (
     <div className="main-content">
+      <TopBar/>
       <FeedNavigation />
       <Feed/>
     </div>
