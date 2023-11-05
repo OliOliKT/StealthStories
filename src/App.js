c4dcddbfa4b253dffe3f1dd4de6723fe2a45ede7
BuurPrime <sofieamaliebuur@gmail.com>
2023-11-05 12:11:11 +0100

    Few additions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import CommentComponent from "./components/CommentComponent";
 import TopBar from "./components/topBarComponent";
 
 import IndvidualPost from "./pages/IndividualPost";
+import TrendingFeed  from "./pages/TrendingFeed";
 
 function App() {
   return (
     <div className="App">
-      <IndvidualPost />
+      <TrendingFeed />
     </div>
   );
 }
