650be1e0b1aab041b03df5dfe606811ed3e81b62
emhberg <114058725+emhberg@users.noreply.github.com>
2023-12-07 21:42:55 +0100

    Created Privacy and Safety page

---
+++ src/pages/PrivacyAndSafety.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./PrivacyAndSafety.css";
+
+import TopBarComponent from "../components/topBarComponent";
+import InformationComponent from "../components/Information";
+import Footer from "../components/footer";
+import UserSettingsSidebar from "../components/UserSettingsSidebar";
+
+const PrivacyAndSafety = () => {
+  return (
+    <>
+      <TopBarComponent />
+      <div className="privacy-grid">
+        <UserSettingsSidebar />
+        <h1 className="pri-and-saf-title">Privacy & Safety</h1>
+        <InformationComponent className="information-component">
+          <p className="pri-and-saf-text">
+            At StealthStories, we understand the importance of privacy and the
+            sensitivity of your personal stories. Our platform is built on the
+            foundation of anonymity, ensuring that you can share your
+            experiences without the fear of being identified. We employ robust
+            security measures to protect your anonymity and the integrity of
+            your stories. From encrypted connections to strict data handling
+            policies, we are dedicated to maintaining a secure environment where
+            your voice can be heard without compromise.
+          </p>
+        </InformationComponent>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default PrivacyAndSafety;
