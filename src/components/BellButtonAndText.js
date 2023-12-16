2e0d2f10dfec721670c543bfe124b89e4cf83ebe
BuurPrime <sofieamaliebuur@gmail.com>
2023-12-16 16:07:05 +0100

    Made tooltip for notifications

--- src/components/BellButtonAndText.js
+++ src/components/BellButtonAndText.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 import "./ButtonAndText.css";
+import ToolTip from './ToolTip';
 
 function BellButtonAndText({ onFollow, isFollowed }) {
-  const iconClass = isFollowed ? "fa-solid fa-bell sipped" : "fa-solid fa-bell";
+  const iconClass = isFollowed ? "fa-solid fa-bell followed" : "fa-solid fa-bell";
 
   return (
+    <ToolTip text="Notification feature has not been implemented yet!" >
     <div className="iconAndText" onClick={onFollow}>
       <i className={iconClass}></i>
       <p className="iconText">Follow</p>
     </div>
+    </ToolTip>
   );
 }
 
